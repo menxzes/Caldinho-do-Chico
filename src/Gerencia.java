5e447f29e7bbe183da247d943feda9e989555142
Guilherme Menezes <glhrm276@gmail.com>
2024-10-17 18:15:04 -0300

    Update Gerencia.java

--- src/Gerencia.java
+++ src/Gerencia.java
@@ -1,4 +1,8 @@
-
 public class Gerencia {
+    private String id;
+    private String nome;
 
+    public void gerarRelatorio() {
+        System.out.println("Relatório de vendas gerado.");
+    }
 }
