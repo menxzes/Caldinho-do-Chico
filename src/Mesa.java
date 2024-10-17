07efc74c2f2bda02f727605278e8be1d26a9b569
Guilherme Menezes <glhrm276@gmail.com>
2024-10-17 17:45:34 -0300

    Update Mesa.java

--- src/Mesa.java
+++ src/Mesa.java
@@ -1,21 +1,21 @@
 public class Mesa {
-	 private int idMesa;
-	    private boolean disponivel;
+	private int idMesa;
+	private boolean disponivel;
 
-	    public Mesa(int idMesa) {
-	        this.idMesa = idMesa;
-	        this.disponivel = true;
-	    }
+	public Mesa(int idMesa, boolean disponivel) {
+		this.idMesa = idMesa;
+		this.disponivel = disponivel;
+	}
 
-	    public int getIdMesa() {
-	        return idMesa;
-	    }
+	public int getIdMesa() {
+		return idMesa;
+	}
 
-	    public boolean isDisponivel() {
-	        return disponivel;
-	    }
+	public boolean isDisponivel() {
+		return disponivel;
+	}
 
-	    public void setDisponivel(boolean disponivel) {
-	        this.disponivel = disponivel;
-	        }
+	public void setDisponivel() {
+		this.disponivel = true;
+	}
 }
