1197781df00b0127ce0e5f8acb84aa9736a1713d
Guilherme Menezes <glhrm276@gmail.com>
2024-10-17 18:23:41 -0300

    Update Pedido.java

--- src/Pedido.java
+++ src/Pedido.java
@@ -1,21 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pedido {
     private String id;
-    private String descricao;
+    private List<ItemCardapio> itens;
+    private float valorTotal;
 
-    public Pedido(String id, String descricao) {
+    public Pedido(String id) {
         this.id = id;
-        this.descricao = descricao;
+        this.itens = new ArrayList<>();
+        this.valorTotal = 0;
     }
 
-    public String getDescricao() {
-        return descricao;
+    public void adicionarItem(ItemCardapio item) {
+        itens.add(item);
+        valorTotal += item.getPreco();
+        System.out.println(item.getNome() + " adicionado à comanda. Total: R$" + valorTotal);
     }
 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
+    public float getValorTotal() {
+        return valorTotal;
     }
 
-    public void isReady() {
-        System.out.println("Pedido pronto para ser servido.");
+    public void exibirItens() {
+        System.out.println("Itens na comanda:");
+        for (ItemCardapio item : itens) {
+            System.out.println("- " + item);
+        }
+        System.out.println("Valor total: R$" + valorTotal);
     }
+
+//    public void isReady() {
+//        System.out.println("Pedido pronto para ser servido.");
+//    }
 }
