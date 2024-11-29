bc8953eb1c824241f0147c08e75e2d589b66dfa3
antoniohbriel <antonioogabrielpps@gmail.com>
2024-11-29 17:31:35 -0300

    Update Visual SistemaDeImpressao

--- src/model/SistemaDeImpressao.java
+++ src/model/SistemaDeImpressao.java
@@ -3,9 +3,9 @@ package model;
 public class SistemaDeImpressao {
 
 	public static void imprimirComanda(Pedido pedido, Atendente atendente, GestaoFinanceira gestaoFinanceira) {
-		System.out.println("\n--- IMPRESSÃO DA COMANDA ---");
-		System.out.println("Atendente: " + atendente.getNome() + " (ID: " + atendente.getId() + ")");
-		System.out.println("Pedido: " + pedido.getValorTotal());
+		System.out.println("\n\033[1;33m=== IMPRESSÃO DA COMANDA ===\033[0m");
+		System.out.println("\033[33mAtendente: " + atendente.getNome() + " (ID: " + atendente.getId() + ")");
+		System.out.println("Pedido: " + pedido.getValorTotal() + "\033[0m");
 
 		pedido.exibirItens();
 		gestaoFinanceira.adicionarReceita(pedido);
